ff53ce7eb22c6f7af5b22c5df13e96a6e3b324df
Parul Jain <pjain@LABA.PSWAS.LOCAL>
2023-06-12 14:05:51 -0700

    Testing the server by making few amends

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>I just learnt about Spirulina.</h1>
+        <h1>I just learnt about Spirulina. It is weird!</h1>
       </header>
     </div>
   );
